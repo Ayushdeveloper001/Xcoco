a5a5dc3c3f9278162b17e51d6a6ca091d1411cd8
Ayush <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-03-04 10:51:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,20 +14,20 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94764497078'] //ur owner number
-global.ownername = "Sandaru Nethsara" //ur owner name
-global.ytname = "YT: Sandaru Yt" //ur yt chanel name
-global.socialm = "GitHub: Mr-Katussa" //ur github or insta name
-global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
+global.owner = ['918260496931'] //ur owner number
+global.ownername = "Ayush" //ur owner name
+global.ytname = "YT: Ayush" //ur yt chanel name
+global.socialm = "GitHub: Ayush op" //ur github or insta name
+global.location = "India, Odisa, Cuttack" //ur location
 
 //bot bomdy 
-global.ownernomer = "94764497078" //ur number
-global.premium = ['94764497078'] //ur premium number
-global.botname = 'Xcoco' //ur bot name
+global.ownernomer = "918260496931" //ur number
+global.premium = ['918260496931'] //ur premium number
+global.botname = 'kya bolu bot' //ur bot name
 global.themeemoji = "♻️" //ur theme emoji
-global.packname = "Sticker By Xcoco" //ur sticker watermark packname
-global.author = "Xcoco" //ur sticker watermark author
-global.wm = "XCOCO BOT INC" //ur watermark
+global.packname = "Mai toh lagda hu" //ur sticker watermark packname
+global.author = "Mujhe kya" //ur sticker watermark author
+global.wm = "Kya bolu bot" //ur watermark
 
 // Other
 global.sessionName = 'session'
